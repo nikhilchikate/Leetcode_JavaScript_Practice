ab8c146ddf0420b1e229dc36ff169789725356ad
nikhilchikate <nikhilchikate07@gmail.com>
2024-10-09 00:00:08 +0530

    updated code

--- 2635_Apply_Transform_Over_Each_Element_in_Array.js
+++ 2635_Apply_Transform_Over_Each_Element_in_Array.js
@@ -37,9 +37,8 @@
  * @return {number[]}
  */
 var map = function (arr, fn) {
-    let resArr = []
     for (let i = 0; i < arr.length; i++) {
-        resArr.push(fn(arr[i], i))
+        arr[i] = fn(arr[i], i);
     }
-    return resArr
+    return arr
 };
